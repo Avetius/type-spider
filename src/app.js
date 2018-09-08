90e25dd30157a75f61db6cd6ad6cc1dda74a4dd4
Avetius <avet.sargsyan@gmail.com>
2018-09-08 17:32:36 +0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var App1 = (function () {
+    function App1() {
+    }
+    App1.prototype.printasd = function () {
+        console.log('hello vzgou');
+        console.log('hello gagou');
+        console.log('hello gagou');
+        console.log('hello gagou');
+        console.log('hello gagou');
+    };
+    return App1;
+}());
+exports.App1 = App1;
+;
+var app = new App1;
+app.printasd();
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+export class App1{
+    constructor(){}
+
+    public printasd (){
+        console.log('hello vzgou');
+        console.log('hello gagou');
+        console.log('hello gagou');
+        console.log('hello gagou');
+        console.log('hello gagou');
+    }
+};
+
+const app = new App1;
+app.printasd();
