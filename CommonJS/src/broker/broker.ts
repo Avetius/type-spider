5121343e4d5c6af2f9b078ebd373d79ca54c9820
avet <avet.sargsyan@dataowl.io>
2018-11-01 11:14:05 +0400

    mac version

--- CommonJS/src/broker/broker.ts
+++ CommonJS/src/broker/broker.ts
@@ -1,4 +1,18 @@
-
+import { queues, connection } from './init';
 export class Broker {
-
+  listen (queueName, cb) {
+    return queues[queueName].activateConsumer((message) => {
+      var controllersMsg = message.getContent();
+      console.log('controllersMsg -> ', controllersMsg);
+      controllersMsg.sendBack = 'controllers'
+      // return fibonacci number
+      return controllersMsg;
+    }, {noAck: true});
+  }
+  send (queueName, message) {
+    return queues[queueName].rpc(message).then(function(result) {
+      console.log(' From "users" queue got ', result.getContent());
+      return result;
+    });
+  }
 } 
